3cb7458e4be0ac554353fcd094c2ab250a5e218a
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-04-12 16:28:15 -0500

    add fft wrapper

---
+++ include/nurfana/FFT.h
@@ -0,0 +1,22 @@
+#ifndef _NURFANA_FFT_H
+#define _NURFANA_FFT_H
+
+/** Thread-safe FFTW3 interface */ 
+
+namespace nurfana
+{
+  namespace fft 
+  {
+    /** Sets a wisdom file. Wisdom will be loaded from and saved to this file */ 
+    void setWisdomFile(const char * f); 
+
+    /** Forward FFT */ 
+    int forward(size_t N, const double * y, std::complex<double> * Y); 
+
+    /** Inverse FFT. N is the time domain length (i.e. Y is N/2+1 long) */ 
+    int inverse(size_t N, const std::complex<double> * Y, double * y); 
+  }
+}
+
+
+#endif
---
+++ src/FFT.cc
@@ -0,0 +1,76 @@
+#include "nurfana/FFT.h" 
+#include <fftw3.h> 
+#include "TMutex.h" 
+#include <map> 
+
+
+static const char * wisdom = nullptr; 
+static TMutex fftw_lock; 
+
+static const int NALIGN = 8; 
+
+struct fft_setup
+{
+  fft_setup(int n) 
+  {
+
+    N  = n;
+    mem = fftw_malloc(sizeof(double * (N + N % NALIGN) + (N/2 + 1) * sizeof(fftw_complex))); 
+    y = (double *) mem; 
+    Y = (fftw_complex *) (y + N + N % NALIGN); 
+
+    TLockGuard l(&fftw_lock); 
+    forward = fftw_plan_dft_r2c_1d(N, y, Y,FFTW_MEASURE | FFTW_DESTROY_INPUT); 
+    inverse = fftw_plan_dft_c2r_1d(N, y, Y,FFTW_MEASURE | FFTW_DESTROY_INPUT); 
+  }
+
+  size_t N; 
+  fftw_plan forward; 
+  fftw_plan inverse; 
+  void * mem; 
+  double * y; 
+  fftw_complex * Y; 
+}; 
+
+static thread_local map<int, fft_setup> setups; 
+
+static void on_exit() __attribute__((destructor)) 
+{
+  if (wisdom) fftw_export_wisdom_to_file(wisdom); 
+}
+
+static fft_setup & setup(size_t N) 
+{
+  if (setups.count(N)) return setups[N]; 
+  setups[N] = fft_setup(N); 
+  return setups[N]; 
+}
+
+namespace nurfana
+{
+  namespace fft
+  {
+    void setWisdomFile(const char * f) 
+    { 
+      wisdom = f; 
+      fftw_import_wisdom_from_filename(f); 
+    }
+
+    int forward(size_t N, const double * y, std::complex<double> *Y) 
+    {
+      fft_setup & s = setup(N); 
+      memcpy(s.y, y, N * sizeof(double)); 
+      fftw_execute(s.forward); 
+      memcpy(Y, s.Y, (N/2 + 1) * sizeof(fftw_complex)); 
+    }
+
+    int inverse(size_t N, const std::complex<double> * Y, double * y); 
+    {
+      fft_setup & s = setup(N); 
+      memcpy(s.Y, Y, (N/2 + 1) * sizeof(fftw_complex)); 
+      fftw_execute(s.inverse); 
+      memcpy(y, s.y, N * sizeof(double)); 
+    }
+  }
+}
+
