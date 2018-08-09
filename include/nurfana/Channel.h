23001409b396030c3c0b688ea5d3d7c4cdb193c7
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-08-09 01:43:19 -0500

    no longer own antenna, response, add digitizer

--- include/nurfana/Channel.h
+++ include/nurfana/Channel.h
@@ -2,13 +2,16 @@
 #define _NURFANA_CHANNEL_H
 
 /**
- * A detector channel . Its main job is to own the memory for the waveform, antenna, and response. 
+ * A detector channel . Its main job is to own the memory for the waveform. 
+ *
+ * It also takes an antenna, response, and digitizer, but since those
+ * are presumably reused a lot, it doesn't own the memory for that. 
  */
 
-#include <memory> 
 #include "nurfana/Waveform.h" 
 #include "nurfana/Antenna.h" 
 #include "nurfana/Response.h" 
+#include "nurfana/Digitizer.h" 
 #include "TNamed.h" 
 
 namespace nurfana
@@ -19,25 +22,31 @@ namespace nurfana
     public: 
       Channel(const char * name,
              Waveform* wf,
-             Antenna*  antenna,
-             Response* response)
+             const Antenna*  antenna,
+             const Response* response,
+             const Digitizer* digitizer
+             )
         : TNamed(name,name) 
       {
         wf_ = wf;
         ant_ = antenna; 
         response_ = response; 
+        digitizer_ = digitizer; 
       }
       
       Waveform * wf() { return wf_; } 
-      Antenna * antenna() { return ant_; } 
-      Response * response() { return response_; } 
+      const Antenna * antenna() const  { return ant_; } 
+      const Response * response() const { return response_; } 
+      const Digitizer * digitizer() const { return digitizer_; }
 
-      virtual ~Channel() { ; }
+      virtual ~Channel() { delete wf_; }
 
     private: 
       Waveform* wf_; 
-      Antenna*  ant_; 
-      Response* response_; 
+      const Antenna*  ant_; 
+      const Response* response_; 
+      const Digitizer* digitizer_; 
+
       ClassDef(Channel,1); 
   }; 
 }
